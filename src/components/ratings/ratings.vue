ae81d4f61cb803d723580fce844b6cd0f71e308d
zts <247931368@qq.com>
2018-11-14 20:50:56 +0800

    新增：ratings组件评价列表，按钮切换仍有问题

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" ref="ratings">
     <div class="ratings-content">
       <div class="overview">
         <div class="overview-left">
@@ -24,26 +24,87 @@
           </div>
         </div>
       </div>
+      <split></split>
+      <ratingselect v-on:childSelectType="childST" v-on:childOnlyContent="childOC" :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.name}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.delivery">{{rating.deliveryTime}}</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
+  import {formatDate} from '../../common/js/date'//export function的带花括号，其他直接export default
   import star from '../../components/star/star'
+  import split from "../../components/split/split"
+  import ratingselect from "../../components/ratingselect/ratingselect"
 
-    export default {
-      props:{
-        seller:{
-          type:Object
-        }
-      },
-      components:{
-        star
+  const ALL = 2
+
+  export default {
+    props:{
+      seller:{
+        type:Object
       }
+    },
+    data(){
+      return{
+        ratings: [],
+        selectType: ALL,
+        onlyContent: true,
+      }
+    },
+    created(){
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body
+        if(response.errno === 0){
+          this.ratings = response.data
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.ratings,{
+            click:true
+            })
+          })
+        }
+      })
+    },
+    filters:{
+      formatDate(time){
+        let date = new Date(time)
+        return formatDate(date,'yyyy-MM-dd hh:mm')
+      }
+    },
+    components:{
+      star,
+      split,
+      ratingselect
     }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .ratings
     position absolute
     top 174px
@@ -112,4 +173,64 @@
             font-size 12px
             color rgb(147,153,159)
 
+    .rating-wrapper
+      padding 0 18px
+      .rating-item
+        display flex
+        padding 18px 0
+        border-1px(rgba(7,17,27,0.1))
+        .avatar
+          flex 0 0 28px
+          width 28px
+          margin-right 12px
+          img
+            border-radius 50%
+        .content
+          flex 1
+          position relative
+          .name
+            margin-bottom 4px
+            line-height 12px
+            font-size 10px
+            color rgb(7,17,27)
+          .star-wrapper
+            margin-bottom 6px
+            font-size 0
+            .star
+              display inline-block
+              vertical-align top
+              margin-right 6px
+            .delivery
+              display inline-block
+              vertical-align top
+              line-height 12px
+              font-size 10px
+              color rgb(147,153,159)
+          .text
+            margin-bottom 8px
+            line-height 18px
+            color rgb(7,17,27)
+            font-size 12px
+          .recommend
+            line-height 16px
+            font-size 0
+            .icon-thumb_up,.item
+              display inline-block
+              margin 0 8px 4px 0
+              font-size 9px
+            .icon-thumb_up
+              color rgb(0,160,220)
+            .item
+              padding 0 6px
+              border 1px solid rgba(7,17,27,0.1)
+              border-radius 1px
+              color rgb(147,153,159)
+              background white
+          .time
+            position absolute
+            top 0
+            right 0
+            line-height 12px
+            font-size 10px
+            color rgb(146,153,159)
 </style>

21b85a5420d836644c1d977762d76d833b23ad98
zts <247931368@qq.com>
2018-11-15 22:24:27 +0800

    新增：seller组件商家公告、实景图、信息等

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -29,20 +29,99 @@
           </li>
         </ul>
       </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item,$index) in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+            <span class="text">{{seller.supports[$index].description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">单家休息</h1>
+        <ul>
+          <li class="info-item" v-for="info in seller.infos">
+            {{info}}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import BScroll from "better-scroll"
   import star from "../../components/star/star"
+  import split from "../../components/split/split"
   export default {
     props:{
       seller:{
         type:Object
       }
     },
+    created(){
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    watch:{
+      "seller"() {
+        this._initScroll()
+        this._initPicscroll()
+      }
+    },
+    mounted(){//dom被渲染过后执行的，也可以在如下的watch中实现(不知为何实现不了)
+      this._initScroll()
+      this._initPicscroll()
+    },
+    methods:{
+      _initScroll(){
+        if(!this.scroll){
+          this.scroll = new BScroll(this.$refs.seller,{
+            click: true
+          })
+        } else{
+          this.scroll.refresh()
+        }
+      },
+      _initPicscroll(){
+        if(this.seller.pics){
+          let picWidth = 120
+          let margin = 6
+          let width = (picWidth+margin)*this.seller.pics.length - margin
+          this.$refs.picList.style.width = width+'px'
+          //alert(this.$refs.picList.style.width)
+          this.$nextTick(() => {
+            if(!this.picScroll){
+              this.picScroll = new BScroll(this.$refs.picWrapper,{
+                scrollX:true,
+                eventPassthrough:'vertical'
+              })
+            }else{
+              this.picScroll.refresh()
+            }
+          })
+        }
+      }
+    },
     components:{
-      star
+      star,
+      split
     }
   }
 </script>
@@ -98,4 +177,82 @@
             color rgb(7,17,27)
             .stress
               font-size 24px
+    .bulletin
+      padding 18px 18px 0 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        color rgb(7,17,27)
+        font-size 14px
+      .content-wrapper
+        padding 0 12px 16px 12px
+        border-1px(rgba(7,17,27,0.2))
+        .content
+          line-height 24px
+          font-size 12px
+          color rgb(240,20,20)
+      .supports
+        .support-item
+          padding 16px 12px
+          border-1px(rgba(7,17,27,0.2))
+          font-size 0
+          &:last-child
+            border-none()
+          .icon
+            display inline-block
+            width 16px
+            height 16px
+            vertical-align: top
+            margin-right 6px
+            background-size 16px 16px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_4')
+            &.discount
+              bg-image('discount_4')
+            &.guarantee
+              bg-image('guarantee_4')
+            &.invoice
+              bg-image('invoice_4')
+            &.special
+              bg-image('special_4')
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7,17,27)
+    .pics
+      padding 18px
+      .title
+        margin-bottom 12px
+        line-height 14px
+        color rgb(7,17,27)
+        font-size 14px
+      .pic-wrapper
+        width:100%
+        overflow hidden
+        white-space nowrap
+        .pic-list
+          font-size 0
+          .pic-item
+            display inline-block
+            margin-right 6px
+            width 120px
+            height 90px
+            &:last-child
+              margin 0
+    .info
+      padding 18px 18px 0 18px
+      color rgb(7,17,27)
+      .title
+        padding-bottom 12px
+        line-height 14px
+        border-1px(rgba(7,17,27,0.1))
+        font-size 14px
+      .info-item
+        padding 16px 12px
+        line-height 16px
+        border-1px(rgba(7,17,27,0.1))
+        font-size 12px
+        &:last-child
+          border-none
 </style>
